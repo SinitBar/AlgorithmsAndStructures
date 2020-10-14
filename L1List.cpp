61941d91e186c0bcd6ea49cdc37e0bb14ba39a61
SinitBar <varka.sinitsckaya@yandex.ru>
2020-10-14 16:22:57 +0300

    Update L1List.cpp

--- L1List.cpp
+++ L1List.cpp
@@ -8,8 +8,8 @@ element::element()
 }
 element* element::get_next() { return next; }
 string element::get_data() { return data; }
-void element::set_next(element* e) { next = e; }
-void element::set_data(string s) { data = s; }
+void element::set_next(element* new_element) { next = new_element; }
+void element::set_data(string information) { data = information; }
 element::~element() { }
 
 L1List::L1List()
@@ -21,10 +21,10 @@ L1List::L1List()
 element* L1List::get_head() { return head; }
 element* L1List::get_cur() { return cur; }
 element* L1List::get_tail() { return tail; }
-void L1List::set_head(element* e) { head = e; }
-void L1List::set_cur(element* e) { cur = e; }
-void L1List::set_tail(element* e) { tail = e; }
-void L1List::set_next(element* e) { cur->set_next(e); }
+void L1List::set_head(element* head_element) { head = head_element; }
+void L1List::set_cur(element* current) { cur = current; }
+void L1List::set_tail(element* tail_element) { tail = tail_element; }
+void L1List::set_next(element* next_element) { cur->set_next(next_element); }
 void L1List::set_data(string data) { cur->set_data(data); }
 element* L1List::get_next() { return (cur->get_next()); }
 string L1List::get_data() { return (cur->get_data()); }
@@ -38,13 +38,13 @@ bool L1List::isEmpty()
 
 void L1List::push_back(string data)
 {
-	element* e = new element;
-	e->set_data(data);
+	element* new_element = new element;
+	new_element->set_data(data);
 	if (isEmpty())
-		head = cur = tail = e;
+		head = cur = tail = new_element;
 	else
 	{
-		tail->set_next(e);
+		tail->set_next(new_element);
 		tail = tail->get_next();
 		cur = tail;
 	}
@@ -52,13 +52,13 @@ void L1List::push_back(string data)
 
 void L1List::push_front(string data)
 {
-	element* e = new element;
-	e->set_data(data);
+	element* new_element = new element;
+	new_element->set_data(data);
 	if (isEmpty())
-		head = cur = tail = e;
+		head = cur = tail = new_element;
 	else
 	{
-		cur = e; // cur = new elem
+		cur = new_element; // cur = new elem
 		cur->set_next(head); // cur->next = head
 		head = cur; // head = cur
 	}
@@ -71,9 +71,9 @@ void L1List::pop_back()
 		set_cur(get_head()); // cur = head
 		if (get_next() == nullptr) // delete head
 		{
-			element* del = head;
+			element* element_to_delete = head;
 			cur = head = tail = nullptr;
-			delete del;
+			delete element_to_delete;
 		}
 		else
 		{
@@ -81,10 +81,10 @@ void L1List::pop_back()
 			{
 				set_cur(get_next()); // cur = cur->next
 			} // cur = the element before the last existing element
-			element* del = get_next();
+			element* element_to_delete = get_next();
 			cur->set_next(nullptr);
 			tail = cur;
-			delete del;
+			delete element_to_delete;
 		}
 	}
 	else
@@ -98,16 +98,16 @@ void L1List::pop_front()
 		cur = head;
 		if (get_next() == nullptr) // delete head
 		{
-			element* del = head;
+			element* element_to_delete = head;
 			head = cur = tail = nullptr;
-			delete del;
+			delete element_to_delete;
 		}
 		else
 		{
-			element* del = head;
+			element* element_to_delete = head;
 			set_cur(head->get_next());// cur = head->next
 			set_head(cur); // head = cur
-			delete del;
+			delete element_to_delete;
 		}
 	}
 	else
@@ -115,18 +115,18 @@ void L1List::pop_front()
 }
 size_t L1List::get_size()
 {
-	size_t Lsize = 0;
+	size_t List_size = 0;
 	if (!isEmpty())
 	{
-		Lsize = 1;
+		List_size = 1;
 		set_cur(get_head()); // now = head
 		while (get_next() != nullptr) // while cur->next exists
 		{
 			set_cur(get_next()); // cur = cur->next
-			Lsize++;
+			List_size++;
 		} // cur = last existing element
 	}
-	return Lsize;
+	return List_size;
 }
 void L1List::insert(string data, size_t index) // first index = 0
 {
@@ -144,11 +144,11 @@ void L1List::insert(string data, size_t index) // first index = 0
 			index--;
 			set_cur(get_next()); // cur = cur->next
 		} // cur is the element before the future new element
-		element* e = new element;
-		e->set_data(data);
-		e->set_next(get_next()); //e->next = cur->next
-		set_next(e); // cur->next = e
-		set_cur(e);
+		element* new_element = new element;
+		new_element->set_data(data);
+		new_element->set_next(get_next()); //e->next = cur->next
+		set_next(new_element); // cur->next = e
+		set_cur(new_element);
 	}
 }
 
@@ -186,12 +186,12 @@ void L1List::remove(size_t index)
 			index--;
 			set_cur(get_next()); // cur = cur->next
 		} // cur is the element before the deleting element
-		element* e = new element;
-		e = get_next();
-		set_next(e->get_next()); //cur->next = cur->next->next
-		if (e == nullptr)
+		element* element_to_delete = new element;
+		element_to_delete = get_next();
+		set_next(element_to_delete->get_next()); //cur->next = cur->next->next
+		if (element_to_delete == nullptr)
 			tail = cur;
-		delete e;
+		delete element_to_delete;
 	}
 }
 
@@ -218,9 +218,9 @@ void L1List::clear()
 			set_head(get_cur()); // head = cur
 
 		} //cur - the last in the list
-		element* e = get_head();
+		element* element_to_delete = get_head();
 		head = cur = tail = nullptr;
-		delete e;
+		delete element_to_delete;
 	}
 	else
 		throw exception("The List is empty");
@@ -251,7 +251,7 @@ void L1List::push_front(L1List* L) // insertion another list into the begining o
 	if (!L->isEmpty())
 	{
 		push_front(L->get_head()->get_data()); // first in this is the same as the first in L now
-		element* h = head; // now head is a new element because that's how my push_front works
+		element* head_element = head; // now head is a new element because that's how my push_front works
 		L->set_cur(L->get_head());
 		while (L->get_next() != nullptr) // while cur->next exists
 		{
@@ -259,7 +259,7 @@ void L1List::push_front(L1List* L) // insertion another list into the begining o
 			insert(L->get_cur()->get_data(), 1);
 			head = cur; // head is an inserted element - this is made for next iteration
 		}
-		set_head(h);
+		set_head(head_element);
 	}
 }
 
@@ -271,20 +271,20 @@ L1List::~L1List()
 
 int main()
 {
-	L1List a;
-	L1List b;
-	b.push_back("hey");
-	b.push_front("hmmm");
-	b.push_back("have you ever thought that...");
-	a.push_back("maybe");
-	a.push_back("today");
-	a.push_back("is");
-	a.push_back("a very");
-	a.push_front(&b);
-	a.push_back("good");
-	a.push_back("day?");
-	a.print_to_console();
-	a.clear();
-	cout << a.isEmpty() << endl;
+	L1List main_List;
+	L1List to_begining_List;
+	to_begining_List.push_back("hey");
+	to_begining_List.push_front("hmmm");
+	to_begining_List.push_back("have you ever thought that...");
+	main_List.push_back("maybe");
+	main_List.push_back("today");
+	main_List.push_back("is");
+	main_List.push_back("a very");
+	main_List.push_front(&to_begining_List);
+	main_List.push_back("good");
+	main_List.push_back("day?");
+	main_List.print_to_console();
+	main_List.clear();
+	cout << main_List.isEmpty() << endl;
 	return 0;
 }
